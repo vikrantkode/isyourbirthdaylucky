699a5ebfcb1e393639317a18cccb4710599f6c0e
Vikrant <vikrant2812@gmail.com>
2021-10-02 00:57:30 +0530

    modified changes

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-var inputDate = document.querySelector("#date")
+var dateOfBirth = document.querySelector("#date-of-birth");
+var luckyNumber = document.querySelector("#lucky-number");
+var chkNumberButton = document.querySelector("#check-number");
+var outputBox = document.querySelector("#output-box");
+
+chkNumberButton.addEventListener("click", checkBirthDateIsLucky)
+
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Your Birthday is lucky :) ";
+    } else {
+        outputBox.innerText = "Your Birthday is not lucky :( ";
+    }
+}
+
+function checkBirthDateIsLucky() {
+    var dob = dateOfBirth.value;
+    var sum = calculateSum(dob);
+    if (sum && dob) {
+        compareValues(sum, luckyNumber.value);
+    } else {
+        outputBox.innerText = "Please Enter Both The Fields";
+    }
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
