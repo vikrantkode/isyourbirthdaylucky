2a02cdc77bc80cebd9bf2d4db403c828c97d7a9f
Vikrant <vikrant2812@gmail.com>
2021-10-11 10:38:31 +0530

    Logic Updated

--- app.js
+++ app.js
@@ -5,31 +5,33 @@ var outputBox = document.querySelector("#output-box");
 
 chkNumberButton.addEventListener("click", checkBirthDateIsLucky)
 
-function compareValues(sum, luckyNumber) {
-    if (sum % Number(luckyNumber.value) === 0) {
-        outputBox.innerText = "Your Birthday is lucky :) ";
-    } else {
-        outputBox.innerText = "Your Birthday is not lucky :( ";
-    }
-}
+// function compareValues(sum, luckyNumber) {
+//     if (sum % Number(luckyNumber.value) === 0) {
+//         outputBox.innerText = "Your Birthday is lucky :) ";
+//     } else {
+//         outputBox.innerText = "Your Birthday is not lucky :( ";
+//     }
+// }
 
 function checkBirthDateIsLucky() {
     var dob = dateOfBirth.value;
     var sum = calculateSum(dob);
-    if(luckyNumber.value > 0 && dob != ""{
-      if (sum && dob) {
-        compareValues(sum, luckyNumber.value);
+    if(luckyNumber.value > 0 && dob != "")
+    {
+      if (sum % Number(luckyNumber.value) === 0) {
+        outputBox.innerText = "Your Birthday is lucky :) ";
       } else {
-        outputBox.innerText = "Please Enter Both The Fields";
-     } else {
-         
+        outputBox.innerText = "Your Birthday is not lucky :( ";
+     }
+     } else {       
         outputBox.innerText = "Please Enter valid inputs";
 }
 
-function calculateSum(dob) {
+function calculateSum(dob) 
+{
     dob = dob.replaceAll("-", "");
     let sum = 0;
-    for (let index = 0; index < dob.length; index++) {
+    for (index = 0; index < dob.length; index++) {
         sum = sum + Number(dob.charAt(index));
     }
     return sum;
